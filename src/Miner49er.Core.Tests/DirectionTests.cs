30dc4dd4e3b841840d4f42ad913fcb9b0b0ebc6d
JimboMcRackem <jim.batiste@gmail.com>
2026-06-06 11:57:36 +0100

    feat(core): add GridPos and Direction

--- src/Miner49er.Core.Tests/DirectionTests.cs
+++ src/Miner49er.Core.Tests/DirectionTests.cs
@@ -3,9 +3,30 @@ using Xunit;
 
 public class DirectionTests
 {
-    [Fact]
-    public void North_offset_points_up()
+    [Theory]
+    [InlineData(Direction.North, 0, -1)]
+    [InlineData(Direction.East, 1, 0)]
+    [InlineData(Direction.South, 0, 1)]
+    [InlineData(Direction.West, -1, 0)]
+    public void ToOffset_returns_expected(Direction d, int x, int y)
     {
-        Assert.Equal(new GridPos(0, -1), Direction.North.ToOffset());
+        Assert.Equal(new GridPos(x, y), d.ToOffset());
+    }
+
+    [Fact]
+    public void Add_and_operator_plus_agree()
+    {
+        var a = new GridPos(3, 4);
+        Assert.Equal(new GridPos(4, 4), a + Direction.East.ToOffset());
+        Assert.Equal(new GridPos(4, 4), a.Add(Direction.East.ToOffset()));
+    }
+
+    [Fact]
+    public void Distances_are_correct()
+    {
+        var a = new GridPos(0, 0);
+        var b = new GridPos(3, 2);
+        Assert.Equal(5, a.ManhattanTo(b));
+        Assert.Equal(3, a.ChebyshevTo(b));
     }
 }
